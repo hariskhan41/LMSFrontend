1505ee6c86848606184843f5e74745ef8ada6863
Ayesha <ayeshaatif@gmail.com>
2019-08-04 22:39:47 -0700

    All Courses Updation

--- src/app/upload-notes/upload-notes.component.ts
+++ src/app/upload-notes/upload-notes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+// import { UploadFile, UploadInput, UploadOutput } from 'ng-uikit';
+// import { humanizeBytes } from 'ng-uikit-pro-standard';
 
 @Component({
   selector: 'app-upload-notes',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadNotesComponent implements OnInit {
 
-  constructor() { }
+  
 
+  constructor() {
+  }
   ngOnInit() {
   }
 
 }
+
